e33d53c997978fe3167d4037f91b359a54695baf
Asif Altaf <asifaltaf13@gmail.com>
2018-05-16 15:51:42 +0200

    going to old version

--- index.js
+++ index.js
@@ -47,21 +47,6 @@ app.intent('noteTake',
 	if (typeof(note) != "undefined")
 	{
 		response.say("Your note: " + note + " was created.");	
-		
-		con.connect(function(err) 
-		{
-		  if (err)throw err;	
-		 
-		  console.log("Connected!");
-		  //Insert a record in the table:
-		  var sql = "INSERT INTO t_minutes (f_title, f_note) VALUES ('Note from Alexa', 'This note was created magically')";
-		  con.query(sql, function (err, result) 
-		  {
-			if (err) throw err;
-			console.log("1 record inserted");
-		  });
-		});	
-
 	}
 	else
 	{
